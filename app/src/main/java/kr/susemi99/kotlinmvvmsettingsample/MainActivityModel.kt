835df14b3ce3e678207a1539be7f1783e66116f4
진창훈 <susemi99@gmail.com>
2018-11-08 15:56:22 +0900

    add RxKotlin

--- app/src/main/java/kr/susemi99/kotlinmvvmsettingsample/MainActivityModel.kt
+++ app/src/main/java/kr/susemi99/kotlinmvvmsettingsample/MainActivityModel.kt
@@ -3,18 +3,36 @@ package kr.susemi99.kotlinmvvmsettingsample;
 import android.util.Log
 import androidx.databinding.ObservableField
 import androidx.lifecycle.ViewModel
+import io.reactivex.Observable
+import io.reactivex.disposables.Disposable
+import java.util.concurrent.TimeUnit
 
 class MainActivityModel : ViewModel() {
   var title = ObservableField<String>()
+  private var intervalDispose: Disposable? = null
 
   init {
-    title.set("title1")
+    intervalDispose = Observable.interval(1, TimeUnit.SECONDS)
+      .subscribe(
+        {
+          title.set(System.currentTimeMillis().toString())
+        },
+        {
+          it.printStackTrace()
+        })
+
+    title.addOnPropertyChangedCallback(object : androidx.databinding.Observable.OnPropertyChangedCallback() {
+      override fun onPropertyChanged(p0: androidx.databinding.Observable?, p1: Int) {
+        Log.i("APP#", "title: " + title.get())
+      }
+    })
   }
 
   override fun onCleared() {
     super.onCleared()
 
     Log.i("APP#", "=== cleared ===")
+    intervalDispose?.dispose()
   }
 
   fun buttonClicked() {
